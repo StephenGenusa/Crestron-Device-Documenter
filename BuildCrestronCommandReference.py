2bbb7aa45fe63058c107ada3fa63011c8dc701d0
Stephen Genusa <github@genusa.com>
2017-08-24 22:12:16 -0500

    Improved UDP detection. General cleanup

--- BuildCrestronCommandReference.py
+++ BuildCrestronCommandReference.py
@@ -48,6 +48,10 @@ CTP_PORT = 41795
 SSH_PORT = 22
 MAX_RETRIES = 3
 CR = "\r"
+BROADCAST_IP = '255.255.255.255'
+CIP_PORT = 41794
+UDP_MSG = "\x14\x00\x00\x00\x01\x04\x00\x03\x00\x00\x66\x65\x65\x64" + \
+    ("\x00" * 252)
 
 
 class CrestronDeviceDocumenter(object):
@@ -66,6 +70,10 @@ class CrestronDeviceDocumenter(object):
         self.preseed_command_list = []
         self.possible_commands_filename = args.addtestcommands
         self.preseed_commands_filename = "preseed.upc"
+        self.firmwareversion = ""
+        self.console_prompt = ""
+        self.htmldocfilename = ""
+        self.unpublished_commands_filename = ""
         self.args = args
 
     def initialize_run_variables(self):
@@ -78,7 +86,7 @@ class CrestronDeviceDocumenter(object):
         self.htmldocfilename = ""
         self.unpublished_commands_filename = ""
 
-    
+
     def place_on_win_clipboard(self, text_to_clipboard):
         """
         For testing of regular expressions
@@ -91,7 +99,7 @@ class CrestronDeviceDocumenter(object):
         r.update()
         r.destroy()
 
-    
+
     def print_debug_data(self, data, msg):
         """
         debug printing of data
@@ -101,7 +109,7 @@ class CrestronDeviceDocumenter(object):
         print("*" * 35, msg, "*" * 35)
         print("\n")
 
-        
+
     def build_list_of_activeips(self, subnet):
         """
         Build a list of devices that respond to ping for a /24 subnet like 17.1.6.{1}:
@@ -110,52 +118,55 @@ class CrestronDeviceDocumenter(object):
         with open(os.devnull, "wb") as limbo:
             for last_octet in xrange(1, 255):
                 ip = "{0}.{1}".format(subnet, last_octet)
-                result=subprocess.Popen(["ping", "-n", "1", "-w", "200", ip],
-                        stdout=limbo, stderr=limbo).wait()
+                result = subprocess.Popen(["ping", "-n", "1", "-w", "200", ip],
+                         stdout=limbo, stderr=limbo).wait()
                 if not result:
                     self.active_ips_to_check.append(ip)
             if self.active_ips_to_check:
                 print("Located {0} active IPs on subnet".format(len(self.active_ips_to_check)))
 
+
     def build_list_of_crestronips(self):
         """
         Build a list of Crestron devices that respond to a UDP message
         """
-        BROADCAST_IP = '255.255.255.255'
-        CIP_PORT = 41794
-        UDP_MSG = "\x14\x00\x00\x00\x01\x04\x00\x03\x00\x00\x66\x65\x65\x64" + \
-            ("\x00" * 252)
         for iface in netifaces.interfaces():
             if netifaces.AF_INET in netifaces.ifaddresses(iface):
-                if 'addr' in netifaces.ifaddresses(iface)[netifaces.AF_INET][0]:
+                # This works better than current version of Crestron Toolbox's Device Discovery
+                if 'broadcast' in netifaces.ifaddresses(iface)[netifaces.AF_INET][0] and \
+                   'addr' in netifaces.ifaddresses(iface)[netifaces.AF_INET][0]:
                     cur_ip = netifaces.ifaddresses(iface)[netifaces.AF_INET][0]['addr']
+                    loc_bcast_ip = netifaces.ifaddresses(iface)[netifaces.AF_INET][0]['broadcast']
                     print("Testing IP subnet", cur_ip)
-                    udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-                    udp_sock.bind((cur_ip, CIP_PORT))
-                    udp_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-                    udp_sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-                    udp_sock.sendto(UDP_MSG, (BROADCAST_IP, CIP_PORT))
-                    udp_sock.settimeout(2.0)
-                    try:
-                        while True:
-                            data, addr = udp_sock.recvfrom(4096)
-                            search = re.findall ('\x00([a-zA-Z0-9-]{2,30})\x00', data[9:40])
-                            if search:
-                                dev_name = search[0]
-                                dev_ip = addr[0]
-                                if cur_ip != dev_ip and dev_ip not in self.active_ips_to_check and dev_name is not "feed":
-                                    self.active_ips_to_check.append(dev_ip)
-                    except:
-                        pass
-        print("\nLocated a total of", len(self.active_ips_to_check), "Crestron devices")
+                    for bcast_ip in [BROADCAST_IP, loc_bcast_ip]:
+                        udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                        udp_sock.bind((cur_ip, CIP_PORT))
+                        udp_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                        udp_sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+                        udp_sock.sendto(UDP_MSG, (bcast_ip, CIP_PORT))
+                        udp_sock.settimeout(2.0)
+                        try:
+                            while True:
+                                data, addr = udp_sock.recvfrom(4096)
+                                search = re.findall ('\x00([a-zA-Z0-9-]{2,30})\x00', data[9:40])
+                                if search:
+                                    dev_name = search[0]
+                                    dev_ip = addr[0]
+                                    if cur_ip != dev_ip and dev_ip not in self.active_ips_to_check and dev_name is not "feed":
+                                        self.active_ips_to_check.append(dev_ip)
+                        except Exception, e:
+                            #print ("The exception was: %s" % e) //timed out
+                            pass
+        total_dev_count = len(self.active_ips_to_check)
+        print ("\nLocated a total of {0} Crestron".format(total_dev_count), "device" if total_dev_count == 1 else "devices")
 
-                
+
     def open_device_connection(self):
         """
         Open the device connection, attempting port 41795
         """
         SOCKET_TIMEOUT = 5.0
-        
+
         if not self.args.forcessh:
             try:
                 self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -381,7 +392,7 @@ class CrestronDeviceDocumenter(object):
                     if command not in command_list and command <> "Add":
                         command_list.append(command)
                         command_dict[command] = search2[0][2].strip()
-    
+
 
     def get_published_command_list(self):
         """
@@ -566,9 +577,9 @@ class CrestronDeviceDocumenter(object):
         htmlfile.write("<p style=\"font-size:12px\">" + self.firmwareversion + "</p>\n")
         htmlfile.write("<p>" + str(len(self.pub_command_list)) +
                        " normal commands found.&nbsp;")
-                       
+
         if self.hidden_command_list:
-            htmlfile.write("<font color=\"" + special_command_color + "\">" + 
+            htmlfile.write("<font color=\"" + special_command_color + "\">" +
                            str(len(self.hidden_command_list)-len(self.pub_command_list)) +
                            "</font> hidden commands available.&nbsp;")
         if self.unpublished_command_list:
@@ -616,8 +627,7 @@ class CrestronDeviceDocumenter(object):
             self.build_list_of_crestronips()
         elif self.args.autolocateactiveips:
             self.build_list_of_activeips(self.args.autolocateactiveips)
-        for ip in self.active_ips_to_check:
-            self.device_ip_address = ip
+        for self.device_ip_address in self.active_ips_to_check:
             self.initialize_run_variables()
             if self.open_device_connection():
                 if self.get_console_prompt():
@@ -633,32 +643,32 @@ class CrestronDeviceDocumenter(object):
                             if os.path.isfile(os.path.realpath(self.htmldocfilename)):
                                 webbrowser.open_new_tab("file://" + os.path.realpath(self.htmldocfilename))
                     else:
-                        print("Documentation file already found for ", self.console_prompt, ". Overwrite is off.")
-                
-                
+                        print("Documentation file already found for {0}. Overwrite is off.".format(self.console_prompt))
+
+
 if __name__ == "__main__":
     # pylint: disable-msg=C0103
     print("\nStephen Genusa's Crestron Device Command Documentation Builder 1.8\n")
     parser = argparse.ArgumentParser()
     parser.add_argument("-ip", "--iptocheck", help="A single Crestron IP address to build documentation for.")
     parser.add_argument("-fssh", "--forcessh", action="store_true",
-                    help="Force use of SSH rather than CTP 41795")
+                        help="Force use of SSH rather than CTP 41795")
     parser.add_argument("-uid", "--username", default="crestron", type=str,
-                    help="Authentication user name.")
+                        help="Authentication user name.")
     parser.add_argument("-pwd", "--password", default="", type=str,
-                    help="Authentication password.")
+                        help="Authentication password.")
     parser.add_argument("-alc", "--autolocatecrestron", action="store_true",
-                    help="Automatically locate Crestron devices on all connected subnets and build documentation.")
+                        help="Automatically locate Crestron devices on all connected subnets and build documentation.")
     parser.add_argument("-ala", "--autolocateactiveips", default="", type=str,
-                    help="Automatically locate active IPs on a subnet and look for Crestron devices. Example: 174.209.101 as an argument will check 174.209.101.0/24.")
+                        help="Automatically locate active IPs on a subnet and look for Crestron devices. Example: 174.209.101 as an argument will check 174.209.101.0/24.")
     parser.add_argument("-atc", "--addtestcommands", default='',
-                    help="Filename containing additional commands to test for")
+                        help="Filename containing additional commands to test for")
     parser.add_argument("-ow", "--overwrite", action="store_true", default=False,
-                    help="Overwrite doc file if it already exists. Off by default.")
-    args = parser.parse_args()
-    if not args.iptocheck and not args.autolocatecrestron and not args.autolocateactiveips:
+                        help="Overwrite doc file if it already exists. Off by default.")
+    parser_args = parser.parse_args()
+    if not parser_args.iptocheck and not parser_args.autolocatecrestron and not parser_args.autolocateactiveips:
         parser.print_help()
         exit()
-    
-    documenter = CrestronDeviceDocumenter(args)
+
+    documenter = CrestronDeviceDocumenter(parser_args)
     documenter.generate_documentation()
